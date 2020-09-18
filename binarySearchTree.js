da48e8ced735d22630f94edac8c462f05e3af469
Rohan <rohans540@gmail.com>
2020-09-18 23:31:12 +0530

    Documentation added

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,3 +1,7 @@
+//Binary Search Tree implementation 
+//Author: Rohan Sharma
+
+//Node class containing 3 data members: data left and right
 class Node {
     constructor(data) {
         this.data = data;
@@ -6,10 +10,15 @@ class Node {
     };
 };
 
+//BST class implementation using Node class
 class BST {
     constructor() {
         this.root = null;
     };
+
+/* insertNewNode(node, newNode) method takes two parameters i.e. node where the new node is to be 
+added and a newNode which is to be added to the tree this method traverses through the tree 
+and add the newNode to the respective position */
 
     insertNewNode(node, newNode) {
         if(newNode.data < node.data){
@@ -27,6 +36,7 @@ class BST {
         }
     };
 
+//insert() method to insert a new node in the tree using insertNewNode method
     insert(data) {
 
         const newNode = new Node(data);
@@ -37,6 +47,7 @@ class BST {
     }
 };
 
+//Test cases:
 const bst1 = new BST();
 bst1.insert(20);
 bst1.insert(9);
