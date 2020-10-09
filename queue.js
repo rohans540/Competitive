5c2736e8af47cec07a4fe86a3307acc629d74497
Rohan <rohans540@gmail.com>
2020-10-09 22:38:01 +0530

    Test cases added

--- queue.js
+++ queue.js
@@ -3,14 +3,17 @@ class Queue {
         this.items = [];
     };
 
+//isEmpty() method to check if a queue is empty or not
     isEmpty() {
         return this.items.length == 0;
     };
 
+//enqueue(item) method to add/push a new item in our queue
     enqueue(item) {
         this.items.push(item);
     };
 
+//dequeue() method to pop out an element from our queue
     dequeue() {
         if(this.isEmpty()) {
             return "Underflow";
@@ -19,11 +22,23 @@ class Queue {
         }
     };
 
+//front() method to return the first element of our queue (element that can be accessed or deleted)
     front() {
         if(this.isEmpty()) {
             return "Queue is empty";
         } else {
             return this.items[0];
         }
-    }
-}
+    };
+
+};
+
+//Test cases
+
+const q1 = new Queue();
+q1.enqueue(10);
+q1.enqueue(12);
+q1.enqueue(7);
+console.log(q1.front());
+
+
