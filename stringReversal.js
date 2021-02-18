694f25c3cff8375e18646465a085da8b2de23a44
Rohan <rohans540@gmail.com>
2021-02-19 01:07:04 +0530

    Recursive algorithms

--- stringReversal.js
+++ stringReversal.js
@@ -1,13 +1,21 @@
-function revStr(st) {
-    let res = '';
-    if(st.length > 0) {
-        for(let i = st.length-1; i >= 0; --i) {
-            res += st[i];
-        };
-        return res;
+// function revStr(st) {
+//     let res = '';
+//     if(st.length > 0) {
+//         for(let i = st.length-1; i >= 0; --i) {
+//             res += st[i];
+//         };
+//         return res;
+//     } else {
+//         return "Invalid size: 0";
+//     };
+// }
+
+function revStr(str) {
+    if(str.length === 1) {
+        return str[str.length - 1];
     } else {
-        return "Invalid size: 0";
-    };
+        return str[str.length - 1] + revStr(str.slice(0, str.length - 1));
+    }
 }
 
-console.log(revStr("Read this backwards"));
+console.log(revStr("Rohan"));
